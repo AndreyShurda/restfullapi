d7ba2b6e04e8d5c55a64da008a5263032a51e870
AndriiShurda <adriano225@mail.ru>
2017-09-17 10:20:37 +0300

    add datasource config and default controller

---
+++ src/main/java/com/hellokoding/restfulapi/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.hellokoding.restfulapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
